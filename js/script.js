a0d4bc7223157e49eafdd00f3c7e0fbf5c039948
Sk Hasan <skhasan2707@gmail.com>
2025-01-25 13:29:43 +0530

    task push

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$('#course-show-slider').owlCarousel({
+    loop: false,
+    dots: false,
+    nav: true,
+    margin: 16,
+    navText:
+        [
+            '<i class="fa-solid fa-circle-chevron-left"></i>',
+            '<i class="fa-solid fa-circle-chevron-right"></i>'
+        ],
+    autoplay: false,
+    smartSpeed: 250,
+    slideBy: 4,
+    responsive: {
+        0: {
+            items: 2,
+        },
+        380: {
+            items: 3,
+        },
+        480: {
+            items: 4,
+        },
+        576: {
+            items: 5,
+        },
+        768: {
+            items: 6,
+        },
+        992: {
+            items: 8,
+        },
+        1200: {
+            items: 10,
+        }, 
+        1400: {
+            items: 12,
+        }
+    }
+})
